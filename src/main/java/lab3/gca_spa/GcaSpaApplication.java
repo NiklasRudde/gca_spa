a2d83855f60cda3b3c3469df3f07b89ba6fc7f0a
Niklas Rudde <n.rudde@shopware.com>
2022-06-05 21:39:10 +0200

    Initial commit

---
+++ src/main/java/lab3/gca_spa/GcaSpaApplication.java
@@ -0,0 +1,13 @@
+package lab3.gca_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcaSpaApplication.class, args);
+	}
+
+}
